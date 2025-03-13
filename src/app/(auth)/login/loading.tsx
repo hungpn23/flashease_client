86e28300ff9cfe2cc6b654d7635a86fc70ed7593
hungpn23 <hungpn23@gmail.com>
2025-03-13 18:35:36 +0700

    Add Nginx service to Docker Compose local and update configurations; enhance loading component with layout

--- src/app/(auth)/login/loading.tsx
+++ src/app/(auth)/login/loading.tsx
@@ -1,5 +1,10 @@
+import { Container } from "@/components/layouts/container";
 import { TextShimmerWave } from "@/components/ui/text-shimmer-wave";
 
 export default function Loading() {
-  return <TextShimmerWave duration={0.5}>Loading...</TextShimmerWave>;
+  return (
+    <Container>
+      <TextShimmerWave duration={0.5}>Loading...</TextShimmerWave>
+    </Container>
+  );
 }
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -6,3 +6,12 @@ export enum VisibleTo {
   JUST_ME = "just me",
   PEOPLE_WITH_A_PASSCODE = "people with a passcode",
 }
+
+const env = process.env.NODE_ENV;
+export const APP_HOST =
+  env === "development" ? "http://localhost" : "https://phamngochung.id.vn";
+
+export const SERVER_URL =
+  env === "development"
+    ? "http://localhost:3001/api/v1"
+    : "https://phamngochung.id.vn";
